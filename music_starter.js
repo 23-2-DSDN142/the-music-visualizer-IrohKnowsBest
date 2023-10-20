bdff74a84abc4a182fdf3828d951a30d12520af6
IrohKnowsBest <alysjeffries7@hotmail.com>
2023-10-21 11:41:06 +1300

    p

--- music_starter.js
+++ music_starter.js
@@ -10,73 +10,86 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
 
 
 
-  let ringSize = drum+100;
-  let vocalMap = map(vocal, 100, 0, 100, 10);
-  let bassMap = map(bass,100,0, 20,100);
-  let otherMap = map(bass,100,0, 30,80);
-  let drumMap = map(bass,100,0, 90,10);
+let ringSize = drum+100;
+let vocalMap = map(vocal, 100, 0, 100, 10);
+let bassMap = map(bass,100,0, 20,100);
+let otherMap = map(bass,100,0, 30,80);
+let drumMap = map(bass,100,0, 90,10);
 let drumHeight= map(drum,100,0, ringSize/4, height);
 let bassHeight = map(bass,100,0,ringSize/4, height)
 let lineSize = 50
 let lineStart = 0
 let lineEnd = lineStart + lineSize
 
-noFill();
-strokeWeight(2);
-stroke(255);
-  ellipse(width/2, height/2, other+100,other+100);
-  ellipse(width/2, bassHeight, bass+100,bass+100);
-  ellipse(width/2, height/2, vocal+250,vocal+150);
-  ellipse(width/2, drumHeight, ringSize);
 
-  stroke(bass,50,50);
-  // line(lineStart, 480, lineEnd, 480);
+ 
+}
+
+
+
+
+
+
+
+
+// noFill();
+// strokeWeight(2);
+// stroke(255);
+//   ellipse(width/2, height/2, other+100,other+100);
+//   ellipse(width/2, bassHeight, bass+100,bass+100);
+//   ellipse(width/2, height/2, vocal+250,vocal+150);
+//   ellipse(width/2, drumHeight, ringSize);
+
+//   stroke(bass,50,50);
+
+
+ // line(lineStart, 480, lineEnd, 480);
   // line(lineStart, 490, lineEnd, 490);
 
-  for (let i=1; i <= vocalMap; i++){
- let lineStep = i*10;
-  line(lineStart, lineStep, lineEnd, lineStep);
-  }
-  for (let i=1; i <= drumMap; i++){
-  let lineStep = i*10;
- line(lineStart+100, lineStep, lineEnd+100, lineStep);
- }
-  for (let i=1; i <= bassMap; i++){
-  let lineStep = i*10;
-  line(lineStart+50, lineStep, lineEnd+50, lineStep);
-  }
-  for (let i=1; i <= otherMap; i++){
-  let lineStep = i*10;
-  line(lineStart+150, lineStep, lineEnd+150, lineStep);
-  }
-  for (let i=1; i <= vocalMap; i++){
-  let lineStep = i*10;
-  line(lineStart+200, lineStep, lineEnd+200, lineStep);
-  }
-  for (let i=1; i <= drumMap; i++){
-  let lineStep = i*10;
-  line(lineStart+250, lineStep, lineEnd+250, lineStep);
-  }
-  for (let i=1; i <= bassMap; i++){
-  let lineStep = i*10;
-  line(lineStart+300, lineStep, lineEnd+300, lineStep);
-  }
-  for (let i=1; i <= otherMap; i++){
-  let lineStep = i*10;
- line(lineStart+350, lineStep, lineEnd+350, lineStep);
-  }
-  for (let i=1; i <= vocalMap; i++){
-    let lineStep = i*10;
-   line(lineStart+400, lineStep, lineEnd+400, lineStep);
-    }
- for (let i=1; i <= drumMap; i++){
-let lineStep = i*10;
-  line(lineStart+450, lineStep, lineEnd+450, lineStep);
- }
- for (let i=1; i <= bassMap; i++){
-  let lineStep = i*10;
-  line(lineStart+500, lineStep, lineEnd+500, lineStep);
-  }
+//   for (let i=1; i <= vocalMap; i++){
+//  let lineStep = i*10;
+//   line(lineStart, lineStep, lineEnd, lineStep);
+//   }
+//   for (let i=1; i <= drumMap; i++){
+//   let lineStep = i*10;
+//  line(lineStart+100, lineStep, lineEnd+100, lineStep);
+//  }
+//   for (let i=1; i <= bassMap; i++){
+//   let lineStep = i*10;
+//   line(lineStart+50, lineStep, lineEnd+50, lineStep);
+//   }
+//   for (let i=1; i <= otherMap; i++){
+//   let lineStep = i*10;
+//   line(lineStart+150, lineStep, lineEnd+150, lineStep);
+//   }
+//   for (let i=1; i <= vocalMap; i++){
+//   let lineStep = i*10;
+//   line(lineStart+200, lineStep, lineEnd+200, lineStep);
+//   }
+//   for (let i=1; i <= drumMap; i++){
+//   let lineStep = i*10;
+//   line(lineStart+250, lineStep, lineEnd+250, lineStep);
+//   }
+//   for (let i=1; i <= bassMap; i++){
+//   let lineStep = i*10;
+//   line(lineStart+300, lineStep, lineEnd+300, lineStep);
+//   }
+//   for (let i=1; i <= otherMap; i++){
+//   let lineStep = i*10;
+//  line(lineStart+350, lineStep, lineEnd+350, lineStep);
+//   }
+//   for (let i=1; i <= vocalMap; i++){
+//     let lineStep = i*10;
+//    line(lineStart+400, lineStep, lineEnd+400, lineStep);
+//     }
+//  for (let i=1; i <= drumMap; i++){
+// let lineStep = i*10;
+//   line(lineStart+450, lineStep, lineEnd+450, lineStep);
+//  }
+//  for (let i=1; i <= bassMap; i++){
+//   let lineStep = i*10;
+//   line(lineStart+500, lineStep, lineEnd+500, lineStep);
+//   }
 
 
 
@@ -114,4 +127,3 @@ let lineStep = i*10;
   //  textAlign(CENTER);
   //  textSize(vocal);
   //  text(words, width/2, height/3);
-}
